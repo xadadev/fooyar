3303672e8a3adde5029a4631871c0aa7cb076860
Muhmmad wahab <wahabzada00@gmail.com>
2023-03-02 01:29:28 +0500

    'finally completed app and footer section has been added as well. responsive has been done'

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,12 @@
+/* -------- HAMBURGER MENU JS ------- */
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.header__ul');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('show');
+});
+
+/* ------------ SPLIDE JS ----------- */
 
